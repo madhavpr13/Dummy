7096903f70a698ef810793adca1f5a4acb797e59
Madhav <madhavwas@gmail.com>
2018-12-07 23:26:50 +0530

    Committing some Java exercises and programs

--- Section3-Conditionals_and_Methods/Exercises/src/com/section3/exercises/HelloWorld.java
+++ Section3-Conditionals_and_Methods/Exercises/src/com/section3/exercises/HelloWorld.java
@@ -3,6 +3,7 @@ package com.section3.exercises;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println(1+2);
     }
 
     public static int add(int a, int b) {
