d6a2f0e4c3edce0050fbe03c03a11631a575c141
Madhav <madhavwas@gmail.com>
2018-12-07 18:26:35 +0530

    Recommitting HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Adding another line");
+		System.out.println("1+2 is " + (1+2));
 	}
 }
