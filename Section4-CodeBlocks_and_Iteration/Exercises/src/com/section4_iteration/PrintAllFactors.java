2f4cec2bc144202f4f6ceb87234786792b7a8719
Madhav <madhavwas@gmail.com>
2018-12-08 23:11:27 +0530

    Committing Code Exercise 20- Print all factors of a number - Section4-Conditionals_and_Iteration

--- Section4-CodeBlocks_and_Iteration/Exercises/src/com/section4_iteration/PrintAllFactors.java
+++ Section4-CodeBlocks_and_Iteration/Exercises/src/com/section4_iteration/PrintAllFactors.java
@@ -1,13 +1,14 @@
 package com.section4_iteration;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
 
 // Code Exercise 20- Print all factors of a given number
 public class PrintAllFactors {
 
     public static ArrayList<Integer> getFactors(int number) {
         ArrayList<Integer> factorsList = new ArrayList<>();
-        for(int i = 1; i <= number; i++) {
+        for(int i = 2; i < number; i++) {
             if(number % i == 0)
                 factorsList.add(i);
         }
@@ -16,12 +17,17 @@ public class PrintAllFactors {
     }
     public static void main(String[] args) {
 
-        System.out.println("We'll print the factors of a number as an ArrayList");
+        System.out.println("We'll print the factors of a number as an ArrayList excluding 1 and the number itself");
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a positive number: ");
         int number = Math.abs(input.nextInt());
         ArrayList<Integer> factorsList = getFactors(number);
-        System.out.println("The factors of " + number + " are: " + factorsList);
+        System.out.println("The factors of " + number +  " excluding 1 and " + number + " are: " + factorsList);
+        if(!factorsList.isEmpty()) {
+            System.out.println("The largest factor of " + number +  " excluding 1 and " + number + " is: " + Collections.max(factorsList));
+        } else {
+            System.out.println("No factor found");
+        }
 
     }
 }
