531b6d35b1e0815e07d0cc64bbc43c43f8422280
Madhav <madhavwas@gmail.com>
2018-12-08 23:14:26 +0530

    Committing - Print all prime factors of a number - Section4-Conditionals_and_Iteration

--- Section4-CodeBlocks_and_Iteration/Exercises/src/com/section4_iteration/PrimeFactors.java
+++ Section4-CodeBlocks_and_Iteration/Exercises/src/com/section4_iteration/PrimeFactors.java
@@ -1,12 +1,10 @@
 package com.section4_iteration;
 
-import java.util.ArrayList;
-
 public class PrimeFactors {
 
 
     public static void printPrimeFactors(int number) {
-        //assert(number > 1);
+
         if(number == 1) return;
 
         int n = 2;
@@ -18,8 +16,7 @@ public class PrimeFactors {
             System.out.println("Found: " + firstIntDivides);
             printPrimeFactors(number / firstIntDivides);
         }
-        //number = number / n;
-        //printPrimeFactors(number);
+
     }
 
     private static int firstIntegerDivides(int number) {
