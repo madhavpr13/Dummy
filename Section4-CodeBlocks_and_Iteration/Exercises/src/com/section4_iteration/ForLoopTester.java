808ae596c47ab30b17b9c876dc5b89ce6b832b4f
Madhav <madhavwas@gmail.com>
2018-12-08 19:31:08 +0530

    Committing For Loop Tester for Section4-Conditionals_and_Iteration

---
+++ Section4-CodeBlocks_and_Iteration/Exercises/src/com/section4_iteration/ForLoopTester.java
@@ -0,0 +1,8 @@
+package com.section4_iteration;
+
+public class ForLoopTester {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++)
+            System.out.println("Hello, World!!");
+    }
+}
