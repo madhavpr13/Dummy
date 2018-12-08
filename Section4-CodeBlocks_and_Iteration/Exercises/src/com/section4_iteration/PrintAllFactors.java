d6c7e8d26656f70e92f29dc9f8e075dc72f7be33
Madhav <madhavwas@gmail.com>
2018-12-08 20:59:20 +0530

    Committing Code Exercise 20- Print all factors of a number - Section4-Conditionals_and_Iteration

---
+++ Section4-CodeBlocks_and_Iteration/Exercises/src/com/section4_iteration/PrintAllFactors.java
@@ -0,0 +1,26 @@
+package com.section4_iteration;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// Code Exercise 20- Print all factors of a given number
+public class PrintAllFactors {
+
+    public static ArrayList<Integer> getFactors(int number) {
+        ArrayList<Integer> factorsList = new ArrayList<>();
+        for(int i = 1; i <= number; i++) {
+            if(number % i == 0)
+                factorsList.add(i);
+        }
+
+        return factorsList;
+    }
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        int number = Math.abs(input.nextInt());
+        ArrayList<Integer> factorsList = getFactors(number);
+        System.out.println("The factors of " + number + " are: " + factorsList);
+
+    }
+}
